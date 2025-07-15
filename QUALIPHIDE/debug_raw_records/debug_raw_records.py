7f968ebb63271d1b04224c77b24a46cba371291b
Lanqing Yuan <yuanlq@uchicago.edu>
2025-07-15 16:24:44 -0500

    Debug raw_records

---
+++ QUALIPHIDE/debug_raw_records/debug_raw_records.py
@@ -0,0 +1,3 @@
+import straxion
+st = straxion.qualiphide()
+rr = st.make("timeS429", "raw_records", config=dict(daq_input_dir="/Users/lanqingyuan/Documents/GitHub/axioph/QUALIPHIDE/debug_raw_records/timeS429/", record_length=5000000))
